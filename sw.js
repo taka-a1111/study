6cdccfb2d8c5809108b7e9bf2f9569ac01fa1964
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-01 12:31:51 +0900

    ver4.8: top-aligned problem and full writing area in pen mode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-01v */
-var CACHE = "kumonmath-2026-09-01v";
+/* BUILD_TAG: 2026-09-01w */
+var CACHE = "kumonmath-2026-09-01w";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
