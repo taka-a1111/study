751c9aed5f339ae25e48644f217bf4907b740d2c
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-01 16:27:15 +0900

    ver5.6: personal best, stamp card, certificate, praise from parents

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-02d */
-var CACHE = "kumonmath-2026-09-02d";
+/* BUILD_TAG: 2026-09-02e */
+var CACHE = "kumonmath-2026-09-02e";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
