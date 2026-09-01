54a2937c5433e97d0ebeb64c5330f9d7b7a271e4
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-01 17:34:07 +0900

    ver6.1: four rotating celebration creatures, faster voice input

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-02i */
-var CACHE = "kumonmath-2026-09-02i";
+/* BUILD_TAG: 2026-09-02j */
+var CACHE = "kumonmath-2026-09-02j";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
