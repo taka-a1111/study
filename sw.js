7730863b2503bccd707939b7dfddf31c5e13aea0
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-02 01:14:30 +0900

    ver7.7: switchable stamp themes (orb/gem/fruit/flower/coin/character)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03a */
-var CACHE = "kumonmath-2026-09-03a";
+/* BUILD_TAG: 2026-09-03b */
+var CACHE = "kumonmath-2026-09-03b";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
