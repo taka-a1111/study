1bc412c3f48286e08a7ce0260aebfbaa97609ce2
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-01 13:06:53 +0900

    ver5.0: proper chime/buzzer sounds (includes ver4.9 answer card position)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-01x */
-var CACHE = "kumonmath-2026-09-01x";
+/* BUILD_TAG: 2026-09-01y */
+var CACHE = "kumonmath-2026-09-01y";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
