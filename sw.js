7bbbfbe78dbb85a89c73648bc55fd1bf1cadb989
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-01 10:42:44 +0900

    ver3.7: move learner chip to top-right of header

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-01i */
-var CACHE = "kumonmath-2026-09-01i";
+/* BUILD_TAG: 2026-09-01j */
+var CACHE = "kumonmath-2026-09-01j";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
