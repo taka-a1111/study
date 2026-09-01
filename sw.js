75ac3aa849456d269e45e51b692772c9036b0e11
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-01 11:12:35 +0900

    ver4.2: high school levels J-O playable, manual update button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-01n */
-var CACHE = "kumonmath-2026-09-01n";
+/* BUILD_TAG: 2026-09-01q */
+var CACHE = "kumonmath-2026-09-01q";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
