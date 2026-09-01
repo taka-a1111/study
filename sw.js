ca055b11e63056c0af5300910efe71e722e104c5
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-02 08:37:59 +0900

    ver7.8: character evolution, code-lock riddles, richer board, mode icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03b */
-var CACHE = "kumonmath-2026-09-03b";
+/* BUILD_TAG: 2026-09-03c */
+var CACHE = "kumonmath-2026-09-03c";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
