9cfaf096466032939f9446bd838aed7bfc1478b1
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-02 23:44:17 +0900

    ver8.2: visual-novel story, tap-to-count riddles, 10 worlds, prominent back button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03f */
-var CACHE = "kumonmath-2026-09-03f";
+/* BUILD_TAG: 2026-09-03g */
+var CACHE = "kumonmath-2026-09-03g";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
