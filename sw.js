ce59c129ff2c6f29d82093748233b24b42dbea0f
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 08:04:01 +0900

    ver8.6: write answers directly in answer cells while in pen/memo mode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03k */
-var CACHE = "kumonmath-2026-09-03k";
+/* BUILD_TAG: 2026-09-03m */
+var CACHE = "kumonmath-2026-09-03m";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
