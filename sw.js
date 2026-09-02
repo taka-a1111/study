c584fcafd51a35148d0717e24731a05617b2cd90
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-02 21:24:55 +0900

    ver8.0: column arithmetic grid with aligned digits, right-to-left entry, auto carry display

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03d */
-var CACHE = "kumonmath-2026-09-03d";
+/* BUILD_TAG: 2026-09-03e */
+var CACHE = "kumonmath-2026-09-03e";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
