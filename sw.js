3d48194a6a9c761a89b9a7b85e393aa3dfddc316
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 13:55:44 +0900

    ver10.5: detective mode (suspect deduction, spot-the-difference, cipher)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-04f */
-var CACHE = "kumonmath-2026-09-04f";
+/* BUILD_TAG: 2026-09-04g */
+var CACHE = "kumonmath-2026-09-04g";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
