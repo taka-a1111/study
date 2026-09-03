a0b6cf2d7b8ed7c232e2eb58d4429173be1ec774
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 12:36:19 +0900

    ver9.2: unique gradient ids (fix dark orbs), subject switch on map

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03s */
-var CACHE = "kumonmath-2026-09-03s";
+/* BUILD_TAG: 2026-09-03t */
+var CACHE = "kumonmath-2026-09-03t";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
