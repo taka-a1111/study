44461f8dccad4ce566f51e591eb045489181db24
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 13:14:18 +0900

    ver9.9: rename to まいがく, fix header overflow (restores fixed nav)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03z */
-var CACHE = "kumonmath-2026-09-03z";
+/* BUILD_TAG: 2026-09-04a */
+var CACHE = "kumonmath-2026-09-04a";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
