4a9296d25b396871fc3050477f281fb7fb996e03
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 13:36:28 +0900

    ver10.2: Japanese D-O (kanji 4-6, middle school, classical/kanbun/modern essay)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-04c */
-var CACHE = "kumonmath-2026-09-04c";
+/* BUILD_TAG: 2026-09-04d */
+var CACHE = "kumonmath-2026-09-04d";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
