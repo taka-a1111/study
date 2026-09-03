0f3b67a2123873073e30c6a298146aa5f25c6aa4
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 17:12:50 +0900

    ver11.0: refresh current screen after pull, 1min interval, manual refresh on records

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-04k */
-var CACHE = "kumonmath-2026-09-04k";
+/* BUILD_TAG: 2026-09-04m */
+var CACHE = "kumonmath-2026-09-04m";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
