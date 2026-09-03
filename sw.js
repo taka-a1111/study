4a47e134e925f5d33290d53d15e44ee11650a727
taka-a1111 <129671422+taka-a1111@users.noreply.github.com>
2026-09-03 13:04:59 +0900

    ver9.6: subject toggle at top, fixed-height sheet screen, compact mode card

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* BUILD_TAG: 2026-09-03w */
-var CACHE = "kumonmath-2026-09-03w";
+/* BUILD_TAG: 2026-09-03x */
+var CACHE = "kumonmath-2026-09-03x";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./digit_model.json"];
 
